781564610da6bdba5cf94a03c91fe7d7e76271d8
Paul Krause <p.krause@surrey.ac.uk>
2015-08-22 07:59:48 +0800

    Basics of pdf report generation now working

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem "devise"
 gem 'activeadmin', github: 'activeadmin'
 
 # Use Prawn and PrawnTo for pdf report generation
-# gem 'prawn', '~> 2.0.2'
+  gem 'prawn', '~> 2.0.2'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'player' => 'steps#player'
   get 'start/:id' => 'steps#start'
   get 'next_step/:id' => 'steps#show'
-  get 'end_game/:id' => 'steps#end_game'
+  get 'end_game/:id' => 'steps#end_game', as: :end_game
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
