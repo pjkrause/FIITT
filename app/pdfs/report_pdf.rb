bf105d6828d45bf199906a50f31e037791db0dd8
Paul Krause <p.krause@surrey.ac.uk>
2015-08-22 21:42:05 +0800

    more content added to the pdf report, including use of prawn-table

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -1,23 +1,36 @@
 class ReportPdf < Prawn::Document
+
   def initialize(status)
     super(top_margin: 80)
     @status = status
     report_title
-    report_summary
+    move_down 20
+    report_author_date
+    move_down 10
+    t = make_table(indicator_data, header: :true, :column_widths => [200, 100]) do
+      row(0).font_style = :bold
+      column(1).align = :center
+    end
+    t.draw
   end
 
   def report_title
-    text "Report for #{Player.find(@status.player_id).email}", size: 15, style: :bold
+    text "SIMULATION REPORT: #{Game.find(@status.game_id).title}", size: 15, style: :bold, align: :center
   end
 
-  def report_summary
-    move_down 20
-    text "Summary of your run", style: :bold
-    text "You completed the scenario in #{@status.day_no} days"
-    text "External Communication: #{@status.external_communication}"
-    text "Internal Communication: #{@status.internal_communication}"
-    text "Media Perception: #{@status.media_perception}" 
-    text "Public Perception: #{@status.public_perception}"
+  def report_author_date
+    text "For Player: #{Player.find(@status.player_id).email}, with simulation completed at #{@status.updated_at}"
+  end
+
+  def indicator_data
+    [
+      ["Indicator", "Grade"],
+      ["Total simulated days",    @status.day_no],
+      ["External Communication",  @status.external_communication],
+      ["Internal Communication",  @status.internal_communication],
+      ["Media Perception",        @status.media_perception],
+      ["Public Perception",       @status.public_perception]
+    ]
   end
 
 end
