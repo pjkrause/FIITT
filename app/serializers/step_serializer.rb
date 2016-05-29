6ce811a081e9e8a9a17468bd7a06a00e37f2d62f
daveseward <david@manageplaces.com>
2016-05-29 10:33:34 +0100

    adding coords to the serializer

--- app/serializers/step_serializer.rb
+++ app/serializers/step_serializer.rb
@@ -1,6 +1,6 @@
 class StepSerializer < ActiveModel::Serializer
 
-  attributes :id, :decision_table, :status_message, :decisions
+  attributes :id, :decision_table, :status_message, :decisions, :x_position, :y_position
 
   def decisions
     Decision.where(step_id: object.id)
