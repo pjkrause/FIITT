982435dbf4a65dfbbdc4519ff59f10e18c2da30b
daveseward <david@manageplaces.com>
2016-05-29 09:59:43 +0100

    sort out serialization for steps to include decisions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,10 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
-
+    
     respond_to do |format|
-      format.json { render json: { game: @game, steps: @game.steps }, status: :ok }
+      format.json { render json: GameSerializer.new(@game, {}) }
+      #format.json { render json: { game: @game, steps: @game_steps.to_json(:include => :decisions)}, status: :ok }
     end
   end
 end
