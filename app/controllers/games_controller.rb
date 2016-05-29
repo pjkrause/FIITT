a4c8bd47c6c1a14d54a5f8388e45675a8d289a4c
daveseward <david@manageplaces.com>
2016-05-29 11:41:15 +0100

    a json route for updating positions of steps and decisions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,58 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
-    
+
     respond_to do |format|
       format.json { render json: GameSerializer.new(@game, {}) }
       #format.json { render json: { game: @game, steps: @game_steps.to_json(:include => :decisions)}, status: :ok }
     end
+  end
+
+  def update_layout
+    errors = []
+
+    if games_params[:steps]
+      games_params[:steps].each do |step|
+        step = step[1]
+        step_to_update = Step.find(step[:id].to_i)
+        if step_to_update
+          step_to_update.x_position = step[:x_position]
+          step_to_update.y_position = step[:y_position]
+          unless step_to_update.save
+            errors << "step_id #{step[:id]}: #{step_to_update.errors.full_messages}"
+          end
+        else
+          errors << "step_id #{step[:id]}: not found"
+        end
+      end
+    end
+
+    if games_params[:decisions]
+      games_params[:decisions].each do |decision|
+        decision = decision[1]
+        decision_to_update = Decision.find(decision[:id].to_i)
+        if decision_to_update
+          decision_to_update.x_position = decision[:x_position]
+          decision_to_update.y_position = decision[:y_position]
+          unless decision_to_update.save
+            errors << "decision_id #{decision[:id]}: #{decision_to_update.errors.full_messages}"
+          end
+        else
+          errors << "decision_id #{decision[:id]}: not found"
+        end
+      end
+    end
+
+    if errors.count == 0
+      render json: :success
+    else
+      render json: { errors: errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def games_params
+    params.require(:games).permit(:id, steps: [:id, :x_position, :y_position], decisions: [:id, :x_position, :y_position])
   end
 end
