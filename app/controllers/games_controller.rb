dd0e7bcd538329c0e217b4c286c742d42b71c27e
daveseward <david@manageplaces.com>
2016-05-30 10:18:57 +0100

    give us a route to update items through the admin/game edit path

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,66 @@ class GamesController < ApplicationController
     end
   end
 
+  def update
+    errors = []
+    updated_model = ""
+
+    @game = Game.find(params[:id])
+
+    if params[:game]
+      if @game
+        @game.title = game_update_params[:title]
+        @game.description = game_update_params[:description]
+        if @game.save
+          updated_model = "Game"
+        else
+          errors << @game.errors.full_messages
+        end
+      else
+        errors << "Game id #{params[:id]} not found"
+      end
+    end
+
+    if params[:step]
+      @step = Step.find(step_update_params[:id])
+      if @step
+        @step.status_message = step_update_params[:status_message]
+
+        if @step.save
+          updated_model = "Step"
+        else
+          errors << @step.errors.full_messages
+        end
+      else
+        errors << "Step id #{step_update_params[:id]} not found"
+      end
+    end
+
+    if params[:decision]
+      @decision = Decision.find(decision_update_params[:id])
+      if @decision
+        @decision.choice = decision_update_params[:choice]
+
+        if @decision.save
+          updated_model = "Decision"
+        else
+          errors << @decision.errors.full_messages
+        end
+      else
+        errors << "Decision id #{decision_update_params[:id]} not found"
+      end
+    end
+
+
+    if errors.count > 0
+      flash[:error] = errors.to_sentence
+    else
+      flash[:notice] = "#{updated_model} updated"
+    end
+
+    redirect_to edit_admin_game_path(@game)
+  end
+
   def update_layout
     errors = []
 
@@ -22,7 +82,7 @@ class GamesController < ApplicationController
             errors << "step_id #{step[:id]}: #{step_to_update.errors.full_messages}"
           end
         else
-          errors << "step_id #{step[:id]}: not found"
+          errors << "Step id #{step[:id]}: not found"
         end
       end
     end
@@ -81,6 +141,32 @@ class GamesController < ApplicationController
   private
 
   def games_params
-    params.require(:games).permit(:id, :x, :y, :pan_x, :pan_y, :zoom, steps: [:id, :x_position, :y_position], decisions: [:id, :x_position, :y_position])
+    params.require(:games).permit(
+      :id, :x, :y, :pan_x, :pan_y, :zoom,
+      steps: [:id, :x_position, :y_position],
+      decisions: [:id, :x_position, :y_position]
+    )
+  end
+
+  def game_update_params
+    params.require(:game).permit(
+      :id,
+      :title,
+      :description
+    )
+  end
+
+  def step_update_params
+    params.require(:step).permit(
+      :id,
+      :status_message
+    )
+  end
+
+  def decision_update_params
+    params.require(:decision).permit(
+      :id,
+      :choice
+    )
   end
 end
